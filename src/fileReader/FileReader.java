5e828ddde351984f0f4165396dd49e59ea7ee04e
Gabriel <gabrielsouto100d@gmail.com>
2015-05-26 23:45:35 +0100

    starting to read capitals

---
+++ src/fileReader/FileReader.java
@@ -0,0 +1,64 @@
+package fileReader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
+public class FileReader {
+	
+	private String filename;
+	
+	public FileReader(String filename) {
+		// TODO Auto-generated constructor stub
+		this.filename = filename;
+	}
+
+	public ArrayList<ArrayList<Double>> read()
+	{
+		RandomAccessFile File;
+
+		ArrayList<ArrayList<Double>> outer = new ArrayList<ArrayList<Double>>();
+
+		try {
+			File = new RandomAccessFile(filename, "r");
+			String line;
+			while ((line = File.readLine()) != null) {
+
+
+				String[] split = line.split(",");
+				ArrayList<Double> inner = new ArrayList<Double>();
+				for (int i = 0; i<split.length;i++){
+					Double value = Double.parseDouble(split[i]);
+					inner.add(value);
+				}
+				outer.add(inner);
+			}
+			
+			File.close();
+			return outer;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		return null;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	
+	
+	
+
+}
--- src/test/NewtworkTest.java
+++ src/test/NewtworkTest.java
@@ -1,10 +1,9 @@
 package test;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+
 import java.util.ArrayList;
 
 import neural.*;
+import fileReader.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -14,50 +13,81 @@ public class NewtworkTest {
 	@Test
 	public void propagationTest() {
 		Network net = new Network(3,2,0.5,0.5,2);
-		RandomAccessFile File;
-		
-		ArrayList<ArrayList<Double>> outer = new ArrayList<ArrayList<Double>>();
-		
-		try {
-			File = new RandomAccessFile("default_features_1059_tracks.txt", "r");
-			String line;
-		 while ((line = File.readLine()) != null) {
-			 
-			 
-			 String[] split = line.split(",");
-			 ArrayList<Double> inner = new ArrayList<Double>();
-			 for (int i = 0; i<split.length;i++){
-				 Double value = Double.parseDouble(split[i]);
-				 inner.add(value);
-			 }
-			 outer.add(inner);
-}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
+
+
 		double input[] = {1.0, 1.3, 1.5};
-		
+
 		net.frontPropagation(input);
-		
+
 		assertEquals("1st Output Expected: ", 1.0, net.getOutputLayer().listIterator(0).next().getValue(), 1.3);
-		
+
 		assertEquals("1st Output Expected: ", 2.0, net.getOutputLayer().listIterator(0).next().getValue(), 1.3);
-		
-		
+
+
 	}
 	@Test
 	public void initialBiasTest(){
-		
+
 		Neuron n = new Neuron(0);
-		
+
 		System.out.println("bias:" + n.getBias());
 		assertEquals("random bias:",0.5,n.getBias(),0.5);
 	}
+
+
+	@Test
+	public void readTest(){
+
+
+		FileReader f = new FileReader("default_features_1059_tracks.txt");
+
+		ArrayList<ArrayList<Double>> def = f.read();
+
+		for(int i = 0; i < def.size(); i++)
+			assertEquals("Expected size index "+i+" : ", 70, def.listIterator(i).next().size(), 0);
+
+		f.setFilename("default_plus_chromatic_features_1059_tracks.txt");
+
+		ArrayList<ArrayList<Double>> chromatic = f.read();
+		for(int i = 0; i < chromatic.size(); i++)
+			assertEquals("Expected size index "+i+" : ", 118, chromatic.listIterator(i).next().size(), 0);
+
+
+	}
+
+
+	@Test
+	public void createExpected()
+	{
+		FileReader f = new FileReader("default_features_1059_tracks.txt");
+
+		ArrayList<Double []> capitals = new ArrayList<Double []>();
+		ArrayList<ArrayList<Double>> def = f.read();
+
+		
+		
+		for(int i = 0; i < def.size(); i++)
+		{
+			boolean inArray = false;;
+			Double coords[] = {def.listIterator(i).next().listIterator(68).next() , def.listIterator(i).next().listIterator(69).next() };
+			for(int j = 0; j < capitals.size();j++)
+			{
+				if(capitals.listIterator(j).next()[0].equals(coords[0]) && capitals.listIterator(j).next()[1].equals(coords[1]) )
+				{
+					inArray = true;
+					break;
+				}
+
+			}
+
+			if(!inArray)
+				capitals.add(coords);
+		}
+
+		
+		assertEquals("Expected Size Output : ", 33, capitals.size(), 0);
+
+	}
+
 
 }
